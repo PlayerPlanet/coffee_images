41a603dd4fe4765f2305d2a06f9cf7c4a8f61b67
Konsta Kiirikki <konsta.kiirikki@gmail.com>
2025-09-17 14:24:29 +0300

    added possibility to run inference for all images.

--- coffee_images/inference/__main__.py
+++ coffee_images/inference/__main__.py
@@ -9,10 +9,11 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_dir', type=str, default="coffee_images/inference/segnet_weights.pth.tar")
     parser.add_argument('--image_dir', type=str, default="raw_img/")
-    parser.add_argument('--n_img', type=int, default=1)
+    parser.add_argument('--n_img', type=str, default=1)
     args = parser.parse_args()
     model = load_model(path=args.model_dir)
-    images, fnames = open_images(image_dir=args.image_dir, n=args.n_img)
+    n_img = None if args.n_img in [None, "all"] else int(args.n_img)
+    images, fnames = open_images(image_dir=args.image_dir, n=n_img)
     masks = []
     for img in images:
         mask = inference(image=img, model=model)
